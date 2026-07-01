e11a999c142c5058150bc24fe3a70b117e92d7fc
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-06-30 19:40:39 -0500

    coins between five to nine

--- src/handlers/gifts.py
+++ src/handlers/gifts.py
@@ -1,18 +1,24 @@
 from TikTokLive.events import GiftEvent
+import pyautogui
 
-one_coin = ["Rose" , "GG" , "Ice Cream Cone" , "White Rose" , "You're awesome" ,
+one_to_four_coins = ["Rose" , "GG" , "Ice Cream Cone" , "White Rose" , "You're awesome" ,
             "Clap Clap" , "TikTok" , "Guardian Wings" , "Community Heart" , "Love you so much" ,
             "Freestyle" , "A Shard of Hope" , "Glow Stick" , "It's corn" , "Pop" , "Good Job" ,
             "Wink wink" , "Cake Slice" , "Maracas" , "Oldies" , "It's Match Time"]
 
+five_to_nine_coins = ["Finger Heart" , "Wave Firework" , "Spinning Soccer" , "Divine Finger" , "Overreact" , "Name shoutout"]
+
 async def on_gift(event: GiftEvent):
     print(f"{event.user.unique_id} sent a {event.gift.name}")
 
-    for i in one_coin:
+    for i in one_to_four_coins:
         if i == event.gift.name:
+            pyautogui.press("space")
             print(f"The gift is {event.gift.name} and it worked")
-"""
-    if event.gift.name == "Rose":
-        pyautogui.press('space')
-        print ("Space key pressed due to Rose gift!")
-"""
+        else:
+            break
+
+    for e in five_to_nine_coins:
+        if i == event.gift.name:
+            pyautogui.press("enter")
+    
