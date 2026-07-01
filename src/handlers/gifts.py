cebad0759da059612aa54dbca76c6d7cddd440d7
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-01 12:37:30 -0500

    tentonineteen coins

--- src/handlers/gifts.py
+++ src/handlers/gifts.py
@@ -8,6 +8,28 @@ one_to_four_coins = ["Rose" , "GG" , "Ice Cream Cone" , "White Rose" , "You're a
 
 five_to_nine_coins = ["Finger Heart" , "Wave Firework" , "Spinning Soccer" , "Divine Finger" , "Overreact" , "Name shoutout"]
 
+ten_to_nineteen_coins = [
+    "Rosa", "Friendship Necklace", "Slow motion", "Journey Pass", "FANDOM Fan",
+    "Chocolate", "LIVE Ranking Ticket", "Sing Together", "League Ball", "Lucky Pony",
+    "Heart", "EWC", "Style Me Up", "Banana Peel", "Heart Gaze", "Intimacy",
+    "Mind Blown", "Gold Boxing Gloves", "ASMR Time", "Cherry Blossom Bunny",
+    "DJ Vinyl", "I Love You", "Ice Lolly", "Tiny Diny", "Shepherd", "LIVE Island",
+    "Whistle", "Potato with Ice Cream", "LIVE Star", "Panther", "Gutab", "Football",
+    "You're the Best!", "Balloons", "Lucky Pig", "Shamrock", "Drip Brewing",
+    "Fried Chicken", "Hi Bear", "Dolphin", "New York Cab", "Beach Radio",
+    "Meowthumbs Up", "Pork Rice Bowl", "Northern Zongzi", "Southern Zongzi",
+    "Udon-nou", "Fukkachan", "Nebaarukun", "Courage Potion", "Koala", "Dragon Egg",
+    "Furious Fire", "Love Piggy", "Boo", "Pumpkin Latte", "Nightshade Seal",
+    "Whitewind Seal", "Firelight Seal", "Crocodile", "Azura Card Fiery",
+    "Azura Card Tranquil", "Minecraft", "Watermelon", "Koala Refuel", "Little Ghost",
+    "Baby Hippo", "Sakura Mochi", "Go Go Go", "Takatanoyumechan", "Easy Victory",
+    "Kuruppa", "DJ Cactus", "Magnifying Glass",
+    "Bravo!", "Romanian Polenta", "Flower Garland", "Woody",
+    "Buzz Light Year", "Tiny Diny in Love", "My Melody & Kuromi",
+]
+
+twenty_to_twentynine_coins = ["Baby Fox" , "Perfume" , "Applause" , "Cry Laugh" , "Love" , "Made My Day" , "OMG" , "Plus One" , "Hilarious" , "Party"]
+
 async def on_gift(event: GiftEvent):
     print(f"{event.user.unique_id} sent a {event.gift.name}")
 
@@ -21,4 +43,8 @@ async def on_gift(event: GiftEvent):
     for e in five_to_nine_coins:
         if e == event.gift.name:
             pyautogui.press("enter")
+
+    for a in ten_to_nineteen_coins:
+        if a == event.gift.name:
+            pyautogui.press("delete")
     
